74e8c5f6423a578b54c9f98df5cb8336e6de88e4
ImightBeaSamurai <52858814+imightbeasamurai@users.noreply.github.com>
2022-11-23 14:27:55 +0100

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,1 +1,26 @@
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	unsigned long	i;
+	unsigned long	j;
 
+	i = 0;
+	if (ft_strlen(needle) == 0)
+		return ((char *)haystack);
+	if (len == 0)
+		return (NULL);
+	while (haystack[i] && i < len)
+	{
+		j = 0;
+		if (haystack[i] == needle[j])
+		{
+			while (i + j < len && haystack[i + j] == needle[j])
+			{
+				j++;
+				if (needle[j] == '\0')
+					return ((char *)(haystack + i));
+			}
+		}
+		i++;
+	}
+	return (0);
+}
