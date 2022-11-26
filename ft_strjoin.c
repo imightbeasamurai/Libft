8372107ae85428159d0ff26c57521bcbcb5824ca
ImightBeaSamurai <52858814+imightbeasamurai@users.noreply.github.com>
2022-11-26 22:48:30 +0100

    Update ft_strjoin.c
    strjoin change

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -12,51 +12,29 @@
 
 #include "libft.h"
 
-static char	*ft_strcat(char *s1, const char *s2)
+char *ft_strjoin(char const *s1, char const *s2)
 {
-	int	len;
-	int	i;
+        int     i;
+        int     j;
+        char    *s;
 
-	len = ft_strlen(s1);
-	i = 0;
-	while (s2[i])
-		s1[len++] = ((char *)s2)[i++];
-	s1[len] = '\0';
-	return (s1);
-}
-
-static char	*ft_strcpy(char *dest, char *src)
-{
-	int	i;
-
-	i = 0;
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	char	*p;
-	int		len_s1;
-	int		len_s2;
-
-	if (!s1 && !s2)
-		return (NULL);
-	if (!s1)
-		return ((char *)s2);
-	if (!s2)
-		return ((char *)s1);
-	len_s1 = ft_strlen(s1);
-	len_s2 = ft_strlen(s2);
-	p = (char *)malloc(len_s1 + len_s2 + 1);
-	if (!p)
-		return (NULL);
-	ft_strcpy (p, (char *) s1);
-	ft_strcat (p, s2);
-	return (p);
+        if (!s1 || !s2)
+                return (NULL);
+        s = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+        if (!s)
+                return (NULL);
+        i = 0;
+        while (s1[i])
+        {
+                s[i] = s1[i];
+                i++;
+        }
+        j = 0;
+        while (s2[j])
+        {
+                s[i + j] = s2[j];
+                j++;
+        }
+        s[i+j] = '\0';
+        return (s);
 }
