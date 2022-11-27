821e27577b43644468dcd71677703b447ccdcb84
ImightBeaSamurai <52858814+imightbeasamurai@users.noreply.github.com>
2022-11-27 17:20:59 +0100

    Update ft_strjoin.c
    another update

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -18,8 +18,12 @@ char *ft_strjoin(char const *s1, char const *s2)
         int     j;
         char    *s;
 
-        if (!s1 || !s2)
+        if (!s1 && !s2)
                 return (NULL);
+        if (!s1)
+                return ((char *) s2);
+        if (!s2)
+                return ((char *) s1);
         s = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
         if (!s)
                 return (NULL);
